3c3e90bc1b407d5dacd28a932d05b3fd25877488
Nándor Urszuly <ursznandor@users.noreply.github.com>
2017-05-21 15:45:59 +0200

    added main method

--- sitemap-generator-project/src/generator_ni/SitemapGenerator.java
+++ sitemap-generator-project/src/generator_ni/SitemapGenerator.java
@@ -1,5 +1,14 @@
 package generator_ni;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class SitemapGenerator {
+	
+	public static void main(String[] args) throws InterruptedException{
+		
+	}
 
 }
