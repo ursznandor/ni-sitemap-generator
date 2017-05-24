2d1c20f8150586fbd89e2974acca86d1baab3c1c
Nándor Urszuly <ursznandor@users.noreply.github.com>
2017-05-24 12:13:22 +0200

    import selenium drivers

--- sitemap-generator-project/src/SitemapGenerator.java
+++ sitemap-generator-project/src/SitemapGenerator.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SitemapGenerator {
 
