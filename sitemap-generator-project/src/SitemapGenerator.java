1a9e2035bac6e8b46f9495ef60c7e5dcd458968e
Nándor Urszuly <ursznandor@users.noreply.github.com>
2017-05-24 12:17:00 +0200

    added ArrayList

--- sitemap-generator-project/src/SitemapGenerator.java
+++ sitemap-generator-project/src/SitemapGenerator.java
@@ -6,6 +6,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SitemapGenerator {
+	
+		private static ArrayList<String> getLinks(ArrayList<WebElement> elements) {
+		ArrayList<String> level = new ArrayList<>();
+		for (WebElement element : elements) {
+			String url = element.getAttribute("href");
+			if (url != null && url.startsWith("http")) {
+				level.add(element.getAttribute("href"));
+			}
+		}
+		return level;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
