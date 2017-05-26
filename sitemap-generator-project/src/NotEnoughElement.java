b338f546e63d895e6b5f85635a2a79b8ce78697e
ursznandor <ursznandor@gmail.com>
2017-05-26 12:14:24 +0200

    added NotFoundElement exception java

---
+++ sitemap-generator-project/src/NotEnoughElement.java
@@ -0,0 +1,9 @@
+
+public class NotEnoughElement extends Exception{
+	
+	@Override
+    public String toString(){
+        return "Link or level not found! Please add one of these, and try again!";
+    }
+
+}
